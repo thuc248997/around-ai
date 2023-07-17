423db09248de4c732e31b5bae4668c37a4b7cac8
thucpham <thuc248997@gmail.com>
2023-07-17 17:46:11 +0700

    edit next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: false,
   images: {
-    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
+    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com', 'lh3.googleusercontent.com'],
   },
 }
